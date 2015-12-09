cc0916a4858a509709a754e7796fd51504ae0384
Paola Lueiza <paola.lueiza@gmail.com>
2015-12-09 00:22:48 -0300

    subiendo toques finales

--- Gemfile
+++ Gemfile
@@ -27,10 +27,6 @@ gem "devise"
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
-#use for contacts
-gem 'mail_form'
-gem 'simple_form'
-
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,47 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+function getAjax ()  { 
+    try  { 
+        if  ( window . XMLHttpRequest )  { 
+            return  new  XMLHttpRequest (); 
+        }  else  if  ( window . ActiveXObject )  { 
+            try  { 
+                return  new  ActiveXObject ( 'Msxml2.XMLHTTP' ); 
+            }  catch  ( try_again )  { 
+                return  new  ActiveXObject ( 'Microsoft.XMLHTTP' ); 
+            } 
+        } 
+    }  catch  ( fail )  { 
+        return  null ; 
+    } 
+}
+
+función sendMail (a, sujetos)  
+     {var rq = getAjax ();
+
+     si  (rq)  {// El éxito; 
+         intente utilizar una petición Ajax a un script PHP para enviar el
+                     
+
+             rq. onreadystatechange =  función  ()  
+                 {si  (esta. readyState ===  4)  
+                     {si  (esta. estado de> =  400)  {// Error de la solicitud; 
+                         caer de nuevo a cliente de correo electrónico 
+                         ventana. abierto ('mailto:'  + a +  '? subject ='  + encodeURIComponent 
+                     
+                 
+             (tema));}}};
+
+             rq. envíe 
+         (nulo);}  catch  (fallar)  {// No se pudo abrir la solicitud; 
+             caer de nuevo a cliente de correo electrónico 
+             ventana. abierto ('mailto:'  + a +  '? subject ='  + encodeURIComponent 
+         
+     (tema));}}  otro  {// Error al crear la solicitud; 
+         caer de nuevo a cliente de correo electrónico 
+         ventana. abierto ('mailto:'  + a +  '? subject ='  + encodeURIComponent 
+     
+(tema));}}
